9fe8434153e9691b573cc76463befd9733624b73
hyperaj <168776874+hyperaj@users.noreply.github.com>
2024-07-05 07:09:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ LOAD = getenv("LOAD", "").split()
 
 NO_LOAD = getenv("NO_LOAD", "").split()
 
-TOKEN = getenv("TOKEN", "7069105732:AAH5EQKXgovDDJHRVqa5KJJCYqnBWFAUAAg")
+TOKEN = getenv("TOKEN", "7336764515:AAE86QqzfI6-fWw7vn0aQ5SCnEj3ZXBquB4")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
-    "mongodb+srv://musicheysiri:musicsiri123@cluster0.gozblfn.mongodb.net/?appName=Cluster0",
+    "mongodb+srv://musicbot1221:123tecmusic@cluster0.geoo2vm.mongodb.net/?retryWrites=true&w=majority",
 )
