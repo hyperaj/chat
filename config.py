a67985dff01b489ff8fcf1d6f670c67745af8a3f
hyperaj <168776874+hyperaj@users.noreply.github.com>
2024-07-15 13:17:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ LOAD = getenv("LOAD", "").split()
 
 NO_LOAD = getenv("NO_LOAD", "").split()
 
-TOKEN = getenv("TOKEN", "7336764515:AAE86QqzfI6-fWw7vn0aQ5SCnEj3ZXBquB4")
+TOKEN = getenv("TOKEN", "6762321126:AAFxD_C3N0wOPYjengR9piy1_DAkUqCfiH0")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
